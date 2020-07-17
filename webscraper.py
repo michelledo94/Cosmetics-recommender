52c02ad9329b3f925b96e802814faa2f26961a2e
nqdao <nhat.quang.d@gmail.com>
2020-07-17 00:36:17 -0400

    use chromedriver-py

--- webscraper.py
+++ webscraper.py
@@ -4,31 +4,12 @@ import requests
 import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from chromedriver_py import binary_path
 import time
 
 class WebScraper:
     def __init__(self, platform):
-        supported_platforms = {
-                                'Linux': 'bin/chromedriver_linux64/chromedriver',
-                                'Darwin': 'bin/chromedriver_mac64/chromedriver',
-                                'Windows': 'bin/chromedriver_win32/chromedriver.exe',
-                              }
-        driver_path = supported_platforms.get(platform, "Unsupported platform.")
-        if driver_path == 'Unsupported platform.':
-            print('Error:', driver_path)
-            raise Exception('Unsupported platform')
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-setuid-sandbox")
-        options.add_argument("--remote-debugging-port=9222")
-        options.add_argument("--disable-dev-shm-using")
-        options.add_argument("--disable-extensions")
-        options.add_argument("--disable-gpu")
-        options.add_argument("start-maximized")
-        options.add_argument("disable-infobars")
-        options.add_argument("--headless")
-        self.driver = webdriver.Chrome(executable_path=driver_path, options=options)
+        self.driver = webdriver.Chrome(executable_path=binary_path)
 
     def scrape_category_links(navigation_xml):
         links = []
@@ -39,7 +20,16 @@ class WebScraper:
             links.append(url[0].text)
         return links
 
+    def scrape_product_links(self, category_page):
+        links = []
+        self.driver.get(category_page)
+        content = self.driver.page_source
+        print(content)
+        return links
+
+
 if __name__ == '__main__':
-    # ws = WebScraper(platform.system())
-    links = WebScraper.scrape_category_links('https://www.ulta.com/navigation0.xml')
-    print(links)
+    ws = WebScraper(platform.system())
+    category_pages = WebScraper.scrape_category_links('https://www.ulta.com/navigation0.xml')
+    for page in category_pages:
+        product_links = ws.scrape_product_links(page)
