0b69821073d40e7290eea5f5b20596b5aa4545ec
nqdao <nhat.quang.d@gmail.com>
2020-08-09 02:00:44 -0400

    scrape reviews and write to csv file

--- webscraper.py
+++ webscraper.py
@@ -22,6 +22,7 @@ class WebScraper:
         self.site = site
         self.catalog = {}
         if 'ulta' in site:
+            self.store = 'ulta'
             self.categories = WebScraper.ulta_categories
         self.driver = webdriver.Chrome(executable_path=binary_path)
 
@@ -32,7 +33,7 @@ class WebScraper:
         for url in tree:
             for category in self.categories:
                 if self.categories[category] in url[0].text:
-                    links.append(url[0].text)
+                    links.append((category, url[0].text))
 
         return links
 
@@ -50,22 +51,29 @@ class WebScraper:
 
     def scrape_product_details(self, product_page):
         self.driver.get(product_page)
-        brand_name = self.driver.find_element_by_xpath("//div[@class='ProductMainSection__brandName']").find_element_by_tag_name('p').text
-        product_name = self.driver.find_element_by_xpath("//div[@class='ProductMainSection__productName']").find_element_by_tag_name('span').text
-        price = self.driver.find_element_by_xpath("//div[@class='ProductPricingPanel']").find_element_by_tag_name('span').text
-        print(brand_name, product_name, price)
-        ingredient_sect = self.driver.find_element_by_xpath("//div[@class='ProductDetail__ingredients']")
-        ingredients = ingredient_sect.find_element_by_xpath(".//div[@class='ProductDetail__productContent']").get_attribute('textContent')
-        print(ingredients)
+        try:
+            brand = self.driver.find_element_by_xpath("//div[@class='ProductMainSection__brandName']").find_element_by_tag_name('p').text
+            name = self.driver.find_element_by_xpath("//div[@class='ProductMainSection__productName']").find_element_by_tag_name('span').text
+            price = self.driver.find_element_by_xpath("//div[@class='ProductPricingPanel']").find_element_by_tag_name('span').text.split('$')[1]
+            rating = self.driver.find_element_by_xpath("//div[@class='RatingPanel']").find_element_by_xpath(".//label[@class='sr-only']").text
+            ingredient_sect = self.driver.find_element_by_xpath("//div[@class='ProductDetail__ingredients']")
+            ingredients = '"' + ingredient_sect.find_element_by_xpath(".//div[@class='ProductDetail__productContent']").get_attribute('textContent') + '"'
+        except selenium.common.exceptions.NoSuchElementException:
+            return None
+
+        return [brand, name, price, rating, ingredients]
 
 if __name__ == '__main__':
     ws = WebScraper(platform.system(), 'https://www.ulta.com/')
-    try:
-        category_pages = ws.scrape_category_links('https://www.ulta.com/navigation0.xml')
-        for page in category_pages:
-            product_links = ws.scrape_product_links(page)
-            for product in product_links:
-                ws.scrape_product_details(product)
-
-    finally:
-        ws.driver.quit()
+    items = []
+    with open(ws.store + '.csv', 'w') as f:
+        try:
+            category_pages = ws.scrape_category_links('https://www.ulta.com/navigation0.xml')
+            for category, page in category_pages:
+                product_links = ws.scrape_product_links(page)
+                for product in product_links:
+                    details = ws.scrape_product_details(product)
+                    if details:
+                        f.write(','.join([category] + details) +'\n')
+        finally:
+            ws.driver.quit()
